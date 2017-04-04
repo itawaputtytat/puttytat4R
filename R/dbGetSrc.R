2ccf4f2da7722d5fd0041496559a34796d01abe4
itawaputtytat <matthias.graichen@unibw.de>
2017-04-04 15:00:41 +0200

    dbGetSrc input to character

--- R/dbGetSrc.R
+++ R/dbGetSrc.R
@@ -1,5 +1,5 @@
 #' @export
-dbGetSrc <- function(name4dbconn, name4src) {
+dbGetSrc <- function(dbconn, name4src) {
 
   outputFunProc(R)
 
@@ -8,8 +8,10 @@ dbGetSrc <- function(name4dbconn, name4src) {
   txt4query <- paste("SELECT * FROM ", name4src, sep = "")
 
   ## Get data with workaround for UTF-8
-  dbWorkaround_UTF8(name4dbconn)
-  dat <- dbGetQuery(get(name4dbconn), txt4query, encoding = "UTF-8")
+  if(!is.character(dbconn))
+    dbconn <- deparse(substitute(dbconn))
+  dbWorkaround_UTF8(dbconn)
+  dat <- dbGetQuery(get(dbconn), txt4query, encoding = "UTF-8")
 
   ## Output
   outputString(paste("* Query data from source:", name4src))
