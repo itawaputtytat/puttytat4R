0141cb2def53b04fd7fec3d95bda12a1a8087f45
magrai <matthias.graichen@unibw.de>
2017-09-27 14:04:30 +0200

    repaired NAMESPACE using roxygen2

--- R/progressBar100.R
+++ R/progressBar100.R
@@ -1,4 +1,4 @@
 #' @title Progressbar for 100 %
-#' @description Use txtProgressBar with predifined settings (0-100%)
+#' @description Use txtProgressBar with predifined settings 0-100%
 #' @export
 progressBar100 <- txtProgressBar(min = 0, max = 100, style = 3)
